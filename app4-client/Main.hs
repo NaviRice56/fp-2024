01e574c0e7b3e698b47edf4702fa4c2cba913a94
Paulius Navikas <paulius.navikas@mif.stud.vu.lt>
2024-12-15 23:41:58 +0200

    Task 4

--- app4-client/Main.hs
+++ app4-client/Main.hs
@@ -1,9 +1,25 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module Main (main) where
 
 import Data.ByteString
 import Network.Wreq
 import Data.String.Conversions
 import Control.Lens
+import qualified Lib2
+import qualified Lib3
+import Data.IORef
+
+data MyDomainAlgebra next
+  = Load (() -> next)
+  | AddArtwork String (() -> next)
+  | Save (() -> next)
+  deriving (Functor)
+
+type MyDomain = Free MyDomainAlgebra
+
+load :: MyDomain ()
+load = liftF $ Load id
 
 main :: IO ()
 main = do
--- app4-server/Main.hs
+++ app4-server/Main.hs
@@ -11,3 +11,19 @@ main = scotty 3000 $
         b <- body
         liftIO $ putStrLn $ concat ["Request was: ", cs b]
         text $ "This is response"
+
+type AppState = (TVar Lib2.State, Chan Lib3.StorageOp)
+cmd :: String -> StateT AppState IO String
+cmd str = do
+  case Lib2.parse Lib3.parseCommand str of
+    (Left e, _) -> liftIO $ return ("PARSE ERROR: " ++ e)
+    (Right c, "") -> do
+      (st, chan) <- S.get
+      tr <- liftIO $ Lib3.stateTransition st c chan
+      case tr of
+        Left e2 -> return ("ERROR: " ++ e2)
+        Right m -> do
+          case m of
+            Just msg -> return msg
+            Nothing -> return ""
+    (Right _, r) -> return ("PARSE ERROR: string is not fully consumed - " ++ r)
